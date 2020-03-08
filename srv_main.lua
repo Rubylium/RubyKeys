16a433c5a02ee9fe406627401fba64c222eb676a
Rubylium <alexisoko0@gmail.com>
2020-03-08 11:17:09 +0100

    Fix server side

--- srv_main.lua
+++ srv_main.lua
@@ -5,26 +5,14 @@ RegisterNetEvent("RS_KEY:RegisterKey")
 AddEventHandler("RS_KEY:RegisterKey", function(keys)
     local idSteam = GetPlayerIdentifier(source, 0)
     local found = false
-    local table = {}
     for k,v in pairs(KeyTable) do
         if v.id == idSteam then
-            table = v
             found = true
             table.remove(KeyTable, k)
             break
         end
     end
-    if not found then
-        table = {
-            id = idSteam,
-            key = keys,
-        }
-    else
-        for k,v in pairs(keys) do
-            table.insert(table.key, v)
-        end
-    end
-    table.insert(KeyTable, table)
+    table.insert(KeyTable, {id = idSteam, key = keys,})
 end)
 
 
